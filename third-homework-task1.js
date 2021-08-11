7122a358176d65b7a7b2b1d0b276e40c23a7b7e3
Assylzhan <Khamiyev02@gmail.com>
2021-08-11 12:28:05 +0500

    1

---
+++ third-homework-task1.js
@@ -0,0 +1,25 @@
+'use strict'
+
+for (let i = 0; i<=10; ++i) {
+    if (i%2==1) {
+        console.log( i + "  -  нечетное число");
+    }   
+     if (i%2==0 && i!==0) {
+        console.log (i + "  -  четное число");
+    }
+
+    if (i==0) {
+        console.log (i + "  -  это ноль");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
